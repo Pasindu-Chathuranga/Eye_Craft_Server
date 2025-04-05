226dc1f835aeadb5589b1347360dd75e99d18bd0
Anubis-cmyk <it19099132@my.sliit.lk>
2025-04-06 01:32:27 +0530

    server update

--- routes/email-route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const MailController = require("../controller/email-controller");
-
-// Routes for send emails
-router.post("/order_placed", MailController.EmailOrderPlaced); 
-
-module.exports = router;
